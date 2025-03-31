e04177bd443ca4bc9361fc1d189a2180296bb52e
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-31 14:33:16 -0300

    add 002-data-types.lua

---
+++ 002-data-types.lua
@@ -0,0 +1,15 @@
+--[[
+    Data Types
+]]
+
+-- String
+print(type("eps1.0_hellofriend.mov"))   -- Output: string
+
+-- Number
+print(type(21000000))                   -- Output: number
+
+-- Boolean
+print(type(true))                       -- Output: boolean
+
+-- Nil: absence of value
+print(type(nil))                        -- Output: nil
