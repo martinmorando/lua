907753571ac23ecf76460c21c4ec1f99c1574708
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-31 17:40:15 -0300

    add 003-numbers.lua

---
+++ 003-numbers.lua
@@ -0,0 +1,15 @@
+--[[
+    Numbers
+    - Exponentiation: ^ (not **)
+]]
+
+x = 10
+y = 2.1
+
+-- Basic operations
+print(x + y)   -- Output: 12.1
+print(x - y)   -- Output: 7.9
+print(x * y)   -- Output: 21.0
+print(x / y)   -- Output: 4.7619047619048
+print(x % y)   -- Output: 1.6
+print(x ^ 2)   -- Output: 100.0
