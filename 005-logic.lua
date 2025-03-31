50b2710536e40815af5b8f0d31287df3fe4f3811
Martín Morando <66503048+martinmorando@users.noreply.github.com>
2025-03-31 20:45:53 -0300

    add 005-logic.lua

---
+++ 005-logic.lua
@@ -0,0 +1,13 @@
+--[[
+    Logic
+]]
+
+she_says = "The teacher says you won't be able to do it"
+
+if she_says == "The teacher says you won't be able to do it" then
+    i_reply = "The teacher can f*ck off!"
+else
+    i_reply = ""
+end
+
+print(i_reply)
